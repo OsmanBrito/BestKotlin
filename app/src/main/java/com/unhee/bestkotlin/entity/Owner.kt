1e16c30cdb4be98fbb1852782a88557d026309a0
OsmanBrito <osman.gimenes@gmail.com>
2019-10-15 18:33:02 -0300

    Insert entity class

---
+++ app/src/main/java/com/unhee/bestkotlin/entity/Owner.kt
@@ -0,0 +1,18 @@
+package com.unhee.bestkotlin.entity
+
+import androidx.room.Entity
+import com.google.gson.annotations.Expose
+import java.io.Serializable
+
+@Entity(
+    tableName = "owner",
+    primaryKeys = ["id"]
+)
+data class Owner (
+    @Expose
+    val id: Long,
+    @Expose
+    val photo: String,
+    @Expose
+    val name: String
+): Serializable
---
+++ app/src/main/java/com/unhee/bestkotlin/entity/Repository.kt
@@ -0,0 +1,18 @@
+package com.unhee.bestkotlin.entity
+
+import androidx.room.Entity
+import com.google.gson.annotations.Expose
+import java.io.Serializable
+
+@Entity(
+    tableName = "repository",
+    primaryKeys = ["id"]
+)
+data class Repository (
+    @Expose
+    val id: Long,
+    @Expose
+    val stargazersCount: Int,
+    @Expose
+    val owner: Owner
+): Serializable
